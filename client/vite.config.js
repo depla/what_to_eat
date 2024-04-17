a375bad5d832c07dbe9c5d62cb43321bbad84fe2
Dennis La <35586986+depla@users.noreply.github.com>
2024-04-17 11:01:57 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api': 'http://localhost:3000' // Adjust the port if your Express server is running on a different port
-      '/api': 'https://what-to-eat-zeta-nine.vercel.app' // Adjust the port if your Express server is running on a different port
+      '/api': 'http://localhost:3000' // Adjust the port if your Express server is running on a different port
+      // '/api': 'https://what-to-eat-zeta-nine.vercel.app' // Adjust the port if your Express server is running on a different port
     }
   }
 })
