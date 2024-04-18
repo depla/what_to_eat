0277cfd9e23d25b22f8c5ff8caa0280e38cf3981
Dennis La <35586986+depla@users.noreply.github.com>
2024-04-18 16:27:29 -0700

    Update GoogleController.js

--- server/controllers/GoogleController.js
+++ server/controllers/GoogleController.js
@@ -1,30 +1,23 @@
 require("dotenv").config();
-const axios = require('axios');
 const { OAuth2Client } = require('google-auth-library');
 const { PrismaClient } = require('@prisma/client');
-const express = require("express");
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const client = new OAuth2Client(CLIENT_ID);
 const prisma = new PrismaClient();
-const app = express();
-const origin = app.get('env') === "production" ? process.env.FRONT_END_PROD_BASE_URL : process.env.FRONT_END_DEV_BASE_URL;
 
 module.exports.googleLogin = async (req, res) => {
     const { tokenId } = req.body;
-    console.log("trying to log in token id:", tokenId);
     try {
         const ticket = await client.verifyIdToken({
             idToken: tokenId,
             audience: CLIENT_ID,
         });
         const payload = ticket.getPayload();
-        console.log("the payyyy load", payload)
         const user = { name: payload['name'], picture: payload['picture'] }
         // Set HTTP-only cookie with JWT token
         const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
         res.cookie('jwt', tokenId, { httpOnly: true, secure: true, expires: expirationDate, sameSite: "None" });
-        console.log("after cookie set. origin:", origin)
         //Add to DB if new user
         const existingEntry = await prisma.user.findUnique({
             where: {
