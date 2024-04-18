8812a2aaddf1c15dd156dd52055c687d065e1037
Dennis La <35586986+depla@users.noreply.github.com>
2024-04-18 11:44:08 -0700

    Update HomePage.jsx

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import SearchFoodComponent from "../../components/SearchFoodComponent/SearchFoodComponent";
-import foodBackground from "../../assets/hand-drawn-cute-food-frame-background_23-2149602177.jpg"
 import "./HomePage.css"
 
 export default function HomePage() {
