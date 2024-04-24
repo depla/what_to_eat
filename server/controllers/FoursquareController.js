7cd6192563c4d9a7216aa8d50da3234627b706bf
Dennis La <35586986+depla@users.noreply.github.com>
2024-04-24 15:58:55 -0700

    add geocaching to get better search results

--- server/controllers/FoursquareController.js
+++ server/controllers/FoursquareController.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const sdk = require('@api/fsq-developers');
+const axios = require('axios');
 
 const apiKey = process.env.FOURSQUARE_API_KEY;
 const IMAGE_SIZE = '300x300'
@@ -20,17 +21,30 @@ const convertFields = (element) => {
     return converted;
 }
 
+const fetchMapData = async (location) => {
+    return axios.get(`https://api.geocodify.com/v2/geocode?api_key=${process.env.GEOCODIFY_API_KEY}&q=${location}`);
+}
+
 module.exports.getFoursquareRecs = async (req, res) => {
     const { search, location, isOpen } = req.body;
+    const mapData = await fetchMapData(location);
+    const coordinates = mapData?.data.response.features[0]?.geometry.coordinates;
     const metadata = {
         query: search,
-        near: location,
         open_now: isOpen ? true : null,
         sort: 'RELEVANCE',
         limit: '50',
         fields: 'fsq_id,name,photos,website,rating,location',
-
     }
+
+    if (coordinates) {
+        metadata.ll = `${coordinates[1]},${coordinates[0]}`;
+        metadata.radius = 15000;
+    }
+    else {
+        metadata.near = location
+    }
+
     sdk.placeSearch(metadata)
         .then(({ data }) => {
             const businesses = data.results;
@@ -46,7 +60,6 @@ module.exports.getFoursquareRecs = async (req, res) => {
             console.error(err)
             res.status(500).send('Internal Server Error');
         });
-
 }
 
 module.exports.getFoursquareBusiness = async (req, res) => {
